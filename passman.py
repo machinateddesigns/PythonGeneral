70604d31254a53ab9b3ea501c60378b13e02cac3
James Monty-Carbonari <james@machinateddesigns.com>
2026-02-08 15:46:28 -0500

    pwman tutorial had issues
    The instructor needs to test these out before he records them.

--- passman.py
+++ passman.py
@@ -1,13 +1,22 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
-def write_key():
+'''def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
         
-write_key()
+write_key()'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
 
 def view():
     with open("pw.txt", "r") as f:
@@ -15,14 +24,14 @@ def view():
             data = line.rstrip().split("|")
             user, pwd = data.split("|")
 
-            print("Account:", user, "| Password:", pwd)
+            print("Account:", user, "| Password:", fer.decrypt(pwd.encode()).decode())
 
 def add():
     account = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("pw.txt", "a") as f:
-        f.write(account + "|" + pwd + "\n")
+        f.write(account + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to view an existing password or add a new one? [view]/[add]. [q] to Quit ").lower()
