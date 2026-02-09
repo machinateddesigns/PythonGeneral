d6d85b3174968d0eab270dfc488798ac2d4c13e9
James Monty-Carbonari <james@machinateddesigns.com>
2026-02-09 02:12:30 -0500

    math and mad libs

--- madlibs.py
+++ madlibs.py
@@ -1,2 +1,31 @@
-import random
+from pathlib import Path
 
+storypath = Path("d:/github/python/pythongeneral/stories/story.txt")
+
+with open(storypath, "r") as f:
+    story = f.read()
+
+words = set()
+startWord = -1
+targetStart = "<"
+targetEnd = ">"
+
+for i, char in enumerate(story):
+    if char == targetStart:
+        startWord = i
+
+    if char == targetEnd and startWord != -1:
+        word = story[startWord: i + 1]
+        words.add(word)
+        startWord = -1
+
+answers = {}
+
+for word in words:
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
