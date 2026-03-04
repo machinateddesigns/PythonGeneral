187a8fa35b03769477d1de5a9e85f469ebf1e985
James Monty-Carbonari <james@machinateddesigns.com>
2026-03-04 11:39:19 -0500

    Update aimtrainer.py

--- aimtrainer.py
+++ aimtrainer.py
@@ -85,7 +85,10 @@ def draw_top_bar(win, elapsed_time, targets_pressed, misses):
     win.blit(lives_label, (650, 5))
 
 def end_screen(win, elapsed_time, targets_pressed, clicks):
-    win.blit(BG_COLOR)
+    win.fill(BG_COLOR)
+
+    gameover_label = LABEL_FONT.render(f"GAME OVER", 1, "white")
+
     time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "white")
 
     speed = round(targets_pressed / elapsed_time, 1)
@@ -96,10 +99,22 @@ def end_screen(win, elapsed_time, targets_pressed, clicks):
     accuracy = round(targets_pressed / clicks * 100, 1)
     accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}%", 1, "white")
 
-    win.blit(time_label, (5, (HEIGHT/2)-12) )
-    win.blit(speed_label, (200, (HEIGHT/2)-12))
-    win.blit(hits_label, (450, (HEIGHT/2)-12))
-    win.blit(accuracy_label, (650, (HEIGHT/2)-12))
+    win.blit(gameover_label, (get_middle(gameover_label), (HEIGHT/2)-200))
+    win.blit(time_label, (get_middle(time_label), (HEIGHT/2)-100))
+    win.blit(speed_label, (get_middle(speed_label), (HEIGHT/2)))
+    win.blit(hits_label, (get_middle(hits_label), (HEIGHT/2)+100))
+    win.blit(accuracy_label, (get_middle(accuracy_label), (HEIGHT/2)+200))
+
+    pygame.display.update()
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                quit()
+
+def get_middle(surface):
+    return WIDTH / 2 - surface.get_width()/2
 
 def main():
     run = True
@@ -145,13 +160,13 @@ def main():
                 targets_pressed += 1
 
         if misses >= LIVES:
-            pass
+            end_screen(WIN, elapsed_time, targets_pressed, clicks)
 
         draw(WIN, targets)
         draw_top_bar(WIN, elapsed_time, targets_pressed, misses)
         pygame.display.update()
 
-    pygame.quit()
+    #pygame.quit()
 
 if __name__ == "__main__":
     main()
